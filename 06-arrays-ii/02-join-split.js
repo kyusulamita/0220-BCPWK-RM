92d80fc1d92b185b5350ef00d92b17713e9e9ace
Sulamita Morales <sulamitamorales@yahoo.com>
2020-03-03 00:03:37 +0000

    all the things

--- 06-arrays-ii/02-join-split.js
+++ 06-arrays-ii/02-join-split.js
@@ -5,9 +5,12 @@
   @param separator {string} [optional]
   @returns joinedStr {string}
 */
+
 // let names = ["Mr Bear", "Pusheen", "Planty", "Mr Sheep"];
+// console.log(names);
+// console.log(names + '');
 // // join every element with a comma
-// let joinedString = names.join();
+// let joinedString = names.join(' and ');
 // console.log(names);
 // console.log(typeof joinedString);
 // console.log(joinedString);
@@ -52,20 +55,21 @@
 
 // const myStr = "Mr Bear and Pusheen and Planty and Mr Sheep";
 
-// const myArr = myStr.split(' and ')
-// const myArr = myStr.split('')
+// const myArr = myStr.split(' ')
+// const myArr = myStr.split('');
 // console.log(myStr);
 // console.log(myArr);
 
 
 let myStr = 'tacocat';
+
 const myArr = myStr.split('');
 console.log(myArr)
 
-// // // this can't happen if it's a string
+// // // // this can't happen if it's a string
 myArr[0] = 'L';
 myArr[1] = 'o';
 
-console.log(myArr);
+console.log(JSON.stringify(myArr, null, 2));
 const newStr = myArr.join('');
 console.log(newStr);
